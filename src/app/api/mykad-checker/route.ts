e67073d4681a81171af61f354576ac3e8a09e00c
altafxx <altafhasnan@gmail.com>
2023-12-10 01:10:18 +0800

    add: mykad validator & extractor API

---
+++ src/app/api/mykad-checker/route.ts
@@ -0,0 +1,18 @@
+export async function GET(req: any, res: any) {
+    try {
+        const mykad = require('mykad');
+        const id = req.nextUrl.searchParams.get('id')
+
+        if (!mykad.isValid(id)) {
+            return Response.json({ message: "Invalid MyKad format" })
+        }
+
+        const response = mykad.parse(id)
+
+        return Response.json(response)
+    }
+    catch (error) {
+        return Response.json({ error })
+
+    }
+}

0fbb94deaad170636544e647141d18c551079077
altafxx <altafhasnan@gmail.com>
2023-12-14 23:53:56 +0800

    add: realtime bus update

---
+++ src/app/api/public-transports/realtime/rapid-bus-kl/route.ts
@@ -0,0 +1,24 @@
+import GTFSRealtimeBindings from 'gtfs-realtime-bindings';
+
+export async function GET(req: any, res: any) {
+    try {
+        // Sample GTFS-R URL from Malaysia's Open API
+        const URL =
+            'https://api.data.gov.my/gtfs-realtime/vehicle-position/prasarana?category=rapid-bus-kl';
+
+        // Fetch GTFS Realtime feed
+        const response = await fetch(URL)
+
+        if (!response.ok) return Response.json({ message: "API Limit" })
+        const buffer = await response.arrayBuffer();
+
+        const feed = GTFSRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(buffer));
+        const tripUpdates = feed.entity.filter((entity) => entity);
+
+        return Response.json({ tripUpdates })
+    }
+    catch (error) {
+        return Response.json({ error })
+
+    }
+}

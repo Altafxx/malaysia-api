04c32759084960c1f552b0b45ab24f2de31f7fa1
altafxx <altafhasnan@gmail.com>
2023-12-14 17:48:12 +0800

    fix: mykad-checker

--- src/app/api/mykad-checker/route.ts
+++ src/app/api/mykad-checker/route.ts
@@ -1,15 +1,17 @@
-export async function GET(req: any, res: any) {
+export async function GET(req: Request, res: Response) {
+    const { searchParams } = new URL(req.url)
+
     try {
         const mykad = require('mykad');
-        const id = req.nextUrl.searchParams.get('id')
+        const id = searchParams.get('id')
 
-        // if (!mykad.isValid(id)) {
-        //     return Response.json({ message: "Invalid MyKad format" })
-        // }
+        if (!mykad.isValid(id)) {
+            return Response.json({ message: "Invalid MyKad format" })
+        }
 
-        // const response = mykad.parse(id)
+        const response = mykad.parse(id)
 
-        return Response.json(id)
+        return Response.json(response)
     }
     catch (error) {
         return Response.json({ error })

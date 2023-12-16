853d4be3f2fb86d326d5bbb2071a252b3a77f18a
altafxx <altafhasnan@gmail.com>
2023-12-16 20:30:09 +0800

    update: rename etnics to ethnics

--- src/app/ethnics/page.tsx
+++ src/app/ethnics/page.tsx
@@ -9,7 +9,7 @@ export default function ethnics() {
 
     return (
         <div className="text-center">
-            <h1>ethnics</h1>
+            <h1>Ethnics</h1>
             <div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-2 sm:md:grid-cols-1 gap-1 mt-4">
                 {data?.ethnics.map((item: any, index: any) => (
                     <div key={index} className="rounded bg-white/5 py-2 m-1">{item}</div>

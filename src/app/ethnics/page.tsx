515ea2e32a3364732af4c8e3fb23167fe77fb429
altafxx <altafhasnan@gmail.com>
2023-12-16 21:26:43 +0800

    fix: etnic deployment error

--- src/app/ethnics/page.tsx
+++ src/app/ethnics/page.tsx
@@ -1,13 +1,7 @@
-async function getData() {
-    const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/ethnics`)
+import ethnic from '@/dataset/ethnics.json'
 
-    if (!res.ok) throw new Error('Failed to fetch data')
-
-    return res.json()
-}
-
-export default async function Etnics() {
-    const data = await getData();
+export default function ethnics() {
+    const data = ethnic;
 
     if (!Array.isArray(data.ethnics)) {
         return <div className="text-center">Invalid data format</div>;
@@ -15,10 +9,10 @@ export default async function Etnics() {
 
     return (
         <div className="text-center">
-            <h1>Etnics</h1>
+            <h1>Ethnics</h1>
             <div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-2 sm:md:grid-cols-1 gap-1 mt-4">
                 {data?.ethnics.map((item: any, index: any) => (
-                    <div key={index} className="rounded bg-white/5 py-1 m-1">{item}</div>
+                    <div key={index} className="rounded bg-white/5 py-2 m-1">{item}</div>
                 ))}
             </div>
         </div>

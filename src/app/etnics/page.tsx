6194df02587480c208e65bd77ad422d5b21b2be4
altafxx <altafhasnan@gmail.com>
2023-12-16 18:56:16 +0800

    update: etnics ui

--- src/app/etnics/page.tsx
+++ src/app/etnics/page.tsx
@@ -12,7 +12,7 @@ export default function Etnics() {
             <h1>Etnics</h1>
             <div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-2 sm:md:grid-cols-1 gap-1 mt-4">
                 {data?.etnics.map((item: any, index: any) => (
-                    <div key={index} className="rounded bg-white/5 py-1 m-1">{item}</div>
+                    <div key={index} className="rounded bg-white/5 py-2 m-1">{item}</div>
                 ))}
             </div>
         </div>

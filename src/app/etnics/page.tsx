6f72386eb598ae4726f6ba2c8b85d9dcdf6a6ce3
altafxx <altafhasnan@gmail.com>
2023-12-16 17:50:35 +0800

    fix: etnic deployment error

--- src/app/etnics/page.tsx
+++ src/app/etnics/page.tsx
@@ -17,7 +17,7 @@ export default async function Etnics() {
         <div className="text-center">
             <h1>Etnics</h1>
             <div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-2 sm:md:grid-cols-1 gap-1 mt-4">
-                {data.etnics.map((item: any, index: any) => (
+                {data?.etnics.map((item: any, index: any) => (
                     <div key={index} className="rounded bg-white/5 py-1 m-1">{item}</div>
                 ))}
             </div>
